7f9c020dbc4ee7d0de4a8cf41e4af7341de7d5c8
Ngocvudut1995 <ngocvudut1995@gmail.com>
2016-11-28 23:51:11 +0700

    Delete
    Delete

--- WEB_TRACUU/WEB_TRACUU/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WEB_TRACUU.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- WEB_TRACUU/WEB_TRACUU/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace WEB_TRACUU.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
